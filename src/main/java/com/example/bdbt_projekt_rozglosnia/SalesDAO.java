abd31279547f3a7ee3fb4df87c87d1280c3a3fb6
borysszycher <01197638@pw.edu.pl>
2026-01-10 00:24:52 +0100

    Commit

--- src/main/java/com/example/bdbt_projekt_rozglosnia/SalesDAO.java
+++ src/main/java/com/example/bdbt_projekt_rozglosnia/SalesDAO.java
@@ -1,4 +1,41 @@
 package com.example.bdbt_projekt_rozglosnia;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class SalesDAO  {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SalesDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Sale> list() {
+        String sql = "SELECT * FROM SALES";
+
+        List<Sale> listSale = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Sale.class));
+        return listSale;
+    }
+
+    public void save(Sale sale) {
+
+    }
+
+    public Sale get(int id) {
+        return null;
+    }
+
+    public void update(Sale sale) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+
 }
